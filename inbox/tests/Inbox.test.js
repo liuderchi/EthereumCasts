301ab118f165e39baf93eb944ebab83759123721
TC Liu <liuderchi@gmail.com>
2021-12-12 00:12:53 +0800

    chore: L46, ganache provider concept

--- inbox/tests/Inbox.test.js
+++ inbox/tests/Inbox.test.js
@@ -1,3 +1,6 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+
+// inject ganache provider to connect to test network
+const web3 = new Web3(ganache.provider());
