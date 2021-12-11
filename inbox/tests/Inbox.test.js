bd8fb81a493043af827bd17ee9b461bfc942e17d
TC Liu <liuderchi@gmail.com>
2021-12-12 00:37:39 +0800

    chore: L49, fetch test accounts

--- inbox/tests/Inbox.test.js
+++ inbox/tests/Inbox.test.js
@@ -4,3 +4,17 @@ const Web3 = require('web3');
 
 // inject ganache provider to connect to test network
 const web3 = new Web3(ganache.provider());
+
+describe('Inbox', () => {
+  beforeEach(async () => {
+    // get a list of all accounts
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts);
+
+    // pick one account to deploy
+  });
+  it('deploys a contract', () => {
+    // manipulate contract
+    // assertx
+  });
+});
