af92dd6456b4e6109ffcdce262b6d0df9c248e89
TC Liu <liuderchi@gmail.com>
2021-12-11 19:25:04 +0800

    chore: L39, solc

--- inbox/compile.js
+++ inbox/compile.js
@@ -1,1 +1,10 @@
-// compile contracts
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+const res = solc.compile(source, 1);
+
+console.log(res);
