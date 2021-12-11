b1840dc3c9f0dbf0c1a7a659cd125941743d247f
TC Liu <liuderchi@gmail.com>
2021-12-12 00:45:51 +0800

    chore: L50-L55, refactor, deploy, assert get & set

--- inbox/tests/Inbox.test.js
+++ inbox/tests/Inbox.test.js
@@ -1,20 +1,43 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+const compiled = require('../compile');
 
 // inject ganache provider to connect to test network
 const web3 = new Web3(ganache.provider());
 
 describe('Inbox', () => {
+  let inbox;
+  let accounts = [];
+
+  const INIT_MESSAGE = 'Hi there!';
+
   beforeEach(async () => {
     // get a list of all accounts
-    const accounts = await web3.eth.getAccounts();
-    console.log(accounts);
+    accounts = await web3.eth.getAccounts();
 
     // pick one account to deploy
+    inbox = await new web3.eth.Contract(JSON.parse(compiled.interface))
+      .deploy({
+        data: compiled.bytecode,
+        arguments: [INIT_MESSAGE],
+      })
+      .send({ from: accounts[0], gas: 1000000 });
   });
+
   it('deploys a contract', () => {
-    // manipulate contract
-    // assertx
+    assert.ok(inbox.options.address);
+  });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INIT_MESSAGE);
+  });
+
+  it('can change the message', async () => {
+    const NEXT_MESSAGE = 'bye';
+    await inbox.methods.setMessage(NEXT_MESSAGE).send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, NEXT_MESSAGE);
   });
 });
