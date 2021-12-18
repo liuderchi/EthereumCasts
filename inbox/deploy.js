733d5853d4689c8b5d060f395ffb045a4c3a3b51
TC Liu <liuderchi@gmail.com>
2021-12-18 16:00:36 +0800

    chore: L56-L59, test deploy to rinkeby with infura API
    - wallet provider api https://github.com/trufflesuite/truffle/tree/develop/packages/hdwallet-provider#general-usage

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,1 +1,11 @@
-// deploy contracts using compiled contracts
+require('dotenv').config();
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider({
+  mnemonic: process.env.MNEMONIC_TEST,
+  providerOrUrl: process.env.WALLET_PROVIDER_TEST,
+});
+const web3 = new Web3(provider);
