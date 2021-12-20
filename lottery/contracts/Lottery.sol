c5a85121fbc14802f8cb2da63e99385c3d2c49ac
TC Liu <liuderchi@gmail.com>
2021-12-21 02:46:03 +0800

    chore: L71-L74, pass sender in constructor
    - bump compiler

--- lottery/contracts/Lottery.sol
+++ lottery/contracts/Lottery.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.11;
 
 contract Lottery {
     address public manager;
 
     constructor() {
-        // TODO
+        manager = msg.sender;
     }
 }
