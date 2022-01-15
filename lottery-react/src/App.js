c5211ba1647b89a6c9e0632364ec0e3b855915e5
liuderchi <liuderchi@gmail.com>
2022-01-15 11:57:56 +0800

    chore: L109-L111, form ui and submit flow

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -9,6 +9,10 @@ function App() {
   const [players, setPlayers] = useState([]);
   const [balance, setBalance] = useState('');
 
+  // form states
+  const [value, setValue] = useState('');
+  const [message, setMessage] = useState('');
+
   useEffect(() => {
     const fetchData = async () => {
       if (web3) {
@@ -27,6 +31,27 @@ function App() {
     fetchData();
   }, []);
 
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+    const account = accounts[0];
+
+    if (value && account) {
+      try {
+        setMessage('Waiting on transaction success...');
+        const transactionResult = await lottery.methods.enter().send({
+          from: account,
+          value: web3.utils.toWei(value, 'ether'),
+        });
+        console.log({ blockHash: transactionResult?.blockHash });
+        setMessage('You have been entered!');
+      } catch (error) {
+        setMessage(`Something wrong! ${error.message}`);
+        console.error(error);
+      }
+    }
+  };
+
   return (
     <div>
       <h2>Lottery Contract</h2>
@@ -35,6 +60,20 @@ function App() {
         {players?.length || 0} people entered, competing to win{' '}
         {web3.utils.fromWei(balance, 'ether')} ether!
       </p>
+      <hr />
+      <form onSubmit={onSubmit}>
+        <h4>Want to try your luck?</h4>
+        <div>
+          <label>Amount of ether to enter (should greater than 0.01) </label>
+          <input
+            value={value}
+            onChange={(event) => setValue(event.target.value)}
+          />
+        </div>
+        <button disabled={!value}>Enter</button>
+      </form>
+      <h1>{message}</h1>
+
       <br />
       <pre>{JSON.stringify({ players, balance }, null, 2)}</pre>
     </div>
